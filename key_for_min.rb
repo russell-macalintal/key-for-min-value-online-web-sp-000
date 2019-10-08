cc452522a7e6f3cbad02dfcdfc071be7d859fe71
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-08 03:00:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    nil
+  else
+    lowest_key = ""
+    lowest_value = 0
+    name_hash.each do |key, value|
+      if value < lowest_value
+        lowest_value = value
+        lowest_key = key
+      end
+    end
+    lowest_key
+  end
 end

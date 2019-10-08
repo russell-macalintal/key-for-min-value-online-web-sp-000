b4348dd646b99d1cf78a3aaa46c23322ecd135ea
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-08 03:08:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   else
-    lowest_key = ""
-    lowest_value = 0
+    lowest_key, lowest_value = hash.first
     name_hash.each do |key, value|
       if value < lowest_value
         lowest_value = value
